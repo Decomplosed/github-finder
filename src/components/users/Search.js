1f60762743bed7400ef73837af5b77038c828c1e
Bart Frackiewicz <decomplosed@gmail.com>
2019-11-12 23:11:06 +0100

    finished the functionality of searching the users from github api

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
 
 export class Search extends Component {
   state = {
     text: ''
   }
 
-  onChange = (e) => this.setState({ [e.target.name]: e.target.value })
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired
+  }
 
-  onSubmit = (e) => {
+  onChange = e => this.setState({ [e.target.name]: e.target.value })
+
+  onSubmit = e => {
     e.preventDefault()
+    this.props.searchUsers(this.state.text)
+    this.setState({ text: '' })
   }
 
   render() {
