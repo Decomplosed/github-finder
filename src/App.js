d6c526ac3838e675d512a65d03e7fd1955f2ea40
Bart Frackiewicz <decomplosed@gmail.com>
2019-11-18 07:32:28 +0100

    started implementing the reducers and context inside App main file and Search component

--- src/App.js
+++ src/App.js
@@ -64,7 +64,6 @@ const App = () => {
               <Route exact path='/' render={props => (
                 <Fragment>
                   <Search
-                    searchUsers={searchUsers}
                     clearUsers={clearUsers}
                     showClear={users.length > 0 ? true : false}
                     setAlert={showAlert}
